eb9103bb47ee4bd316e6bf0edbecf7ed92fd27d0
James gitere <sasmuypwa2019.com>
2024-02-27 16:29:50 +0300

    rectified betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,8 @@
 /**
  * binary_tree_delete - deletes an enire binary tree
  * @tree: pinter of the tree to be delete
- * Description: deletes an entire tree starting from root. if the tree is NULL, does nothing
+ * Description: deletes an entire tree starting from root.
+ * if the tree is NULL, does nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
