e8478ec7681fb5789ad6dab399da31daf49221ad
James gitere <sasmuypwa2019.com>
2024-02-29 11:14:38 +0300

    refactored the code

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,25 +1,30 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_leaves - counts the leaves in a binary tree
-* @tree: pointer to the root node of the tree to count the leaves
+* binary_tree_leaves - Counts the leaves in a binary tree
+* @tree: Pointer to the root node of the tree to count the leaves
 *
-* Return: number of leaf nodes in the binary tree, 0 if the is null
+* Return: Number of leaf nodes in the binary tree, 0 if tree is NULL
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t leaves_count = 0;
 
+	/* If tree is NULL, return 0 */
 	if (tree == NULL)
 		return (0);
+
+	/* If the current node is a leaf node, increment the leaves count */
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	leaves += binary_tree_leaves(tree->left);
-	leaves += binary_tree_leaves(tree->right);
+		leaves_count++;
 
-	return (leaves);
+	/* Recursively count the leaves in the left and right subtrees */
+	leaves_count += binary_tree_leaves(tree->left);
+	leaves_count += binary_tree_leaves(tree->right);
 
+	return (leaves_count);
 }
+
 
 
 
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t l, r;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (1 + (left_height > right_height ? left_height : right_height));
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((l > r) ? l : r);
 }
